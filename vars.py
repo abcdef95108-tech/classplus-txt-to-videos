b5eae78aeaf8840594e9c03874ecaf39d65d015f
abcdef95108-tech <abcdef95108@gmail.com>
2025-12-27 17:29:52 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,7 +1,8 @@
-api_id = 25218674
-api_hash = "87c231f0e8704795b8239d06965b4351"
-bot_token = "6326230595:AAHGRQLrdDp-RiXyfNe9veBxwabOju-ldSY"
-auth_users = [6805344720]
-sudo_user = 6805344720
-log_channel = -1002038285084
-txt_channel = -1002038285084
+api_id = "33516244"
+api_hash = "c1abd0630fcd54d33bc6528ec0955fff"
+bot_token = ""
+auth_users = [6220046859]
+sudo_user = 6220046859
+log_channel = -1003532488647
+txt_channel = -1003532488647
+
